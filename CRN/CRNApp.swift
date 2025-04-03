807e6d8459653c882c75bfacef266273793e7395
Grigory Avdyushin <avdyushin.g@gmail.com>
2025-04-03 09:08:58 +0200

    Initial Commit

---
+++ CRN/CRNApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRNApp.swift
+//  CRN
+//
+//  Created by Grigory Avdyushin   on 03/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CRNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
